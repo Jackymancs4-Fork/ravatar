ae854534e2bcfd981e359183790ebc8879d8b155
Giacomo Rossetto <jackyman_cs4@live.it>
2021-08-01 14:35:42 +0200

    Add Svg rendering method

--- index.js
+++ index.js
@@ -166,6 +166,232 @@ Ravatar.renderScaled = function(containerElement, avatarProps, size, scale) {
   Ravatar.render(containerElement, avatarProps, size * scale)
 }
 
+Ravatar.renderSVG = function(containerElement, avatarProps, size) {
+  // https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-on-html-canvas/7838871#7838871
+  function roundRect(context, x, y, w, h, r, color = 'red') {
+
+    let rect = document.createElementNS('http://www.w3.org/2000/svg','rect')
+    rect.setAttributeNS(null, 'x', x)
+    rect.setAttributeNS(null, 'y', y)
+    rect.setAttributeNS(null, 'width', w)
+    rect.setAttributeNS(null, 'height', h)
+    rect.setAttributeNS(null, 'rx', r)
+    rect.setAttributeNS(null, 'ry', r)
+    rect.setAttributeNS(null, 'fill', color)
+
+    context.appendChild(rect)
+
+    return context;
+  }
+
+  function getFillStyle(currentNode, avatarProps) {
+    if (avatarProps.coloredNodes[currentNode]) {
+      return lightLayerGradient;
+    }
+    else if (avatarProps.dark) {
+      return '#f0f0f0';
+    }
+    else {
+      return '#a0a0a0';
+    }
+  }
+
+  containerElement.width = size;
+  containerElement.height = size;
+  let context = document.createElementNS('http://www.w3.org/2000/svg','svg');
+  let defs = document.createElementNS('http://www.w3.org/2000/svg','defs');
+
+  context.setAttributeNS(null, 'width', size)
+  context.setAttributeNS(null, 'height', size)
+
+  context.appendChild(defs)
+
+  // Calculate some stuff
+  const padding = containerElement.width / 6; // 1/6 of each side is padding totalling 1/3 of the canvas width
+  let length = 4 * (containerElement.width / 6); // 4/6 or 2/3 of the area is for the glyph
+  // Take the length and divide into 3 sections since 3 lines
+  // A quarter of each trine is for margin,
+  // but since the margin on the last line is not needed, add that back to the other two margins using *(3/2)
+  const margin = (3 / 2) * (length / 3 / 4);
+  const width = length / 4;
+  // When there's a line and a space on the same row, the length of the line is different
+  // Use (2/3) as a base then subtract half the length of the margin that would be included
+  const halfLineLength = length * (2/3) - ((1/2) * (length / 3 / 4));
+
+  // Render the background gradient
+  if (avatarProps.hasBg) {
+    let backgroundGradient = document.createElementNS('http://www.w3.org/2000/svg','linearGradient')
+    backgroundGradient.id = "backgroundGradient"
+    backgroundGradient.setAttributeNS(null, 'gradientUnits', "userSpaceOnUse")
+
+    backgroundGradient.setAttributeNS(null, 'x1', '0')
+    backgroundGradient.setAttributeNS(null, 'y1', size)
+    backgroundGradient.setAttributeNS(null, 'x2', size)
+    backgroundGradient.setAttributeNS(null, 'y2', '0')
+
+    let firstStopColorBackgroundGradient = document.createElementNS('http://www.w3.org/2000/svg','stop')
+    firstStopColorBackgroundGradient.setAttributeNS(null, 'offset', '0%')
+    firstStopColorBackgroundGradient.setAttributeNS(null, 'stop-opacity', '1')
+    let secondStopColorBackgroundGradient = document.createElementNS('http://www.w3.org/2000/svg','stop')
+    secondStopColorBackgroundGradient.setAttributeNS(null, 'offset', '100%')
+    secondStopColorBackgroundGradient.setAttributeNS(null, 'stop-opacity', '1')
+
+    if (avatarProps.dark) {
+      firstStopColorBackgroundGradient.style.stopColor = 'rgb(0, 0, 0)'
+      secondStopColorBackgroundGradient.style.stopColor = 'rgb(42, 42, 42)'
+    }
+    else {
+      firstStopColorBackgroundGradient.style.stopColor = 'rgb(213, 213, 213)'
+      secondStopColorBackgroundGradient.style.stopColor = 'rgb(255, 255, 255)'
+    }
+
+    backgroundGradient.appendChild(firstStopColorBackgroundGradient)
+    backgroundGradient.appendChild(secondStopColorBackgroundGradient)
+
+    defs.appendChild(backgroundGradient)
+
+    if (avatarProps.roundedBg) {
+      roundRect(context, 0, 0, containerElement.width, containerElement.width, width / 2, "url(#backgroundGradient)");
+    }
+    else {
+      roundRect(context, 0, 0, containerElement.width, containerElement.width, 0, "url(#backgroundGradient)");
+    }
+  }
+
+  if (avatarProps.isVertical) {
+    // Rotate -90deg by vertically skewing up and horizontally skewing right
+    // Since the entire transform is now perfectly off the canvas, bring it back on by translating 512 down
+    context.setAttributeNS(null, 'transform', "rotate(-90)")
+  }
+
+  let darkLayerGradientObject = document.createElementNS('http://www.w3.org/2000/svg','linearGradient')
+  darkLayerGradientObject.id = "darkLayerGradient"
+
+  darkLayerGradientObject.setAttributeNS(null, 'gradientUnits', "userSpaceOnUse")
+
+  darkLayerGradientObject.setAttributeNS(null, 'x1', padding)
+  darkLayerGradientObject.setAttributeNS(null, 'x2', (length + padding))
+  darkLayerGradientObject.setAttributeNS(null, 'y1', 0)
+  darkLayerGradientObject.setAttributeNS(null, 'y2', 0)
+
+  let firstStopColorDarkLayerGradient = document.createElementNS('http://www.w3.org/2000/svg','stop')
+  firstStopColorDarkLayerGradient.setAttributeNS(null, 'offset', '0%')
+  firstStopColorDarkLayerGradient.style.stopColor = avatarProps.gradientSet.bgLow
+  firstStopColorDarkLayerGradient.style.stopOpacity = '1'
+  let secondStopColorDarkLayerGradient = document.createElementNS('http://www.w3.org/2000/svg','stop')
+  secondStopColorDarkLayerGradient.setAttributeNS(null, 'offset', '100%')
+  secondStopColorDarkLayerGradient.style.stopColor = avatarProps.gradientSet.bgHigh
+  secondStopColorDarkLayerGradient.style.stopOpacity = '1'
+
+  darkLayerGradientObject.appendChild(firstStopColorDarkLayerGradient)
+  darkLayerGradientObject.appendChild(secondStopColorDarkLayerGradient)
+
+  let lightLayerGradientObject = document.createElementNS('http://www.w3.org/2000/svg','linearGradient')
+  lightLayerGradientObject.id = "lightLayerGradient"
+  lightLayerGradientObject.setAttributeNS(null, 'gradientUnits', "userSpaceOnUse")
+
+  lightLayerGradientObject.setAttributeNS(null, 'x1', padding)
+  lightLayerGradientObject.setAttributeNS(null, 'x2', (length + padding))
+  lightLayerGradientObject.setAttributeNS(null, 'y1', 0)
+  lightLayerGradientObject.setAttributeNS(null, 'y2', 0)
+
+  let firstStopColorLightLayerGradient = document.createElementNS('http://www.w3.org/2000/svg','stop')
+  firstStopColorLightLayerGradient.setAttributeNS(null, 'offset', '0%')
+  firstStopColorLightLayerGradient.setAttributeNS(null, 'stop-color', avatarProps.gradientSet.low)
+  firstStopColorLightLayerGradient.style.stopOpacity = '1'
+  let secondStopColorLightLayerGradient = document.createElementNS('http://www.w3.org/2000/svg','stop')
+  secondStopColorLightLayerGradient.setAttributeNS(null, 'offset', '100%')
+  secondStopColorLightLayerGradient.setAttributeNS(null, 'stop-color', avatarProps.gradientSet.high)
+  secondStopColorLightLayerGradient.style.stopOpacity = '1'
+
+  lightLayerGradientObject.appendChild(firstStopColorLightLayerGradient)
+  lightLayerGradientObject.appendChild(secondStopColorLightLayerGradient)
+
+  defs.appendChild(darkLayerGradientObject)
+  defs.appendChild(lightLayerGradientObject)
+
+  let darkLayerGradient = "url(#darkLayerGradient)"
+  let lightLayerGradient = "url(#lightLayerGradient)"
+
+  // Keep track of the node for coloring
+  let currentNode = 0;
+
+  // Render the rows of the actual avatar
+  for (var i = 0; i < 3; i++) {
+    let startX = padding;
+    let startY = padding + ((width + margin) * i);
+    switch (avatarProps.rows[i].type) {
+      case 'LINE':
+        roundRect(context, startX, startY, length, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'LINE_DOT':
+        roundRect(context, startX, startY, length, width, width / 2, darkLayerGradient);
+        roundRect(context, startX, startY, halfLineLength, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += halfLineLength + margin;
+
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'DOT_LINE':
+        roundRect(context, startX, startY, length, width, width / 2, darkLayerGradient);
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += width + margin;
+
+        roundRect(context, startX, startY, halfLineLength, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'DOT_DOT_DOT':
+        roundRect(context, startX, startY, length, width, width / 2, darkLayerGradient);
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += width + margin;
+
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += width + margin;
+
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'DOT_SPACE_DOT':
+        roundRect(context, startX, startY, length, width, width / 2, darkLayerGradient);
+
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += 2 * (width + margin);
+
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'DOT_DOT_SPACE':
+        roundRect(context, startX, startY, halfLineLength, width, width / 2, darkLayerGradient);
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += width + margin;
+
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'SPACE_DOT_DOT':
+        startX += width + margin;
+
+        roundRect(context, startX, startY, halfLineLength, width, width / 2, darkLayerGradient);
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+
+        startX += width + margin;
+        roundRect(context, startX, startY, width, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'SPACE_LINE':
+        startX += width + margin;
+        roundRect(context, startX, startY, halfLineLength, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+      case 'LINE_SPACE':
+        roundRect(context, startX, startY, halfLineLength, width, width / 2, getFillStyle(currentNode++, avatarProps));
+        break;
+    }
+  }
+
+  containerElement.appendChild(context)
+
+};
+
 Ravatar.render = function(canvasElement, avatarProps, size) {
   // https://stackoverflow.com/questions/1255512/how-to-draw-a-rounded-rectangle-on-html-canvas/7838871#7838871
   function roundRect(context, x, y, w, h, r) {
